59ad492ea64e3da7af4e10015c63c6ce31242e96
Braian Ledantes <braianledantes@gmail.com>
2023-12-06 10:19:38 -0300

    Item details

---
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -0,0 +1,30 @@
+
+import { useContext } from "react"
+import { CartContext } from "../../context"
+import { Card, Button } from "react-bootstrap"
+import PropTypes from "prop-types"
+
+ItemDetail.propTypes = {
+    product: PropTypes.object
+}
+
+export function ItemDetail({ product }) {
+    const { addItem } = useContext(CartContext)
+
+    function handleAddProduct() {
+        addItem({ product })
+    }
+
+    return (
+        <Card className="item-details">
+            <Card.Img variant="top" src={product.img} />
+            <Card.Body>
+                <Card.Title>{product.name}</Card.Title>
+                <Card.Text>
+                    {product.details}
+                </Card.Text>
+                <Button variant="success" onClick={handleAddProduct}>Agregar al carrito</Button>
+            </Card.Body>
+        </Card>
+    )
+}
--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,31 +1,13 @@
 import { useParams } from "react-router-dom"
 import { useMenuItem } from "../../hooks/useMenu"
 import { Loader } from ".."
-import { Button, Card } from "react-bootstrap"
-import { useContext } from "react"
-import { CartContext } from "../../context"
+import { ItemDetail } from "../ItemDetail/ItemDetail"
 
 export function ItemDetailContainer() {
     const { id } = useParams();
     const { product, loading, error } = useMenuItem(id);
-    const { addItem } = useContext(CartContext);
-
-    function handleAddProduct() {
-        addItem({ product });
-    }
 
     if (loading) return <Loader />
     if (error) return (<div>Ha ocurrido un error {error.error}</div>)
-    return (
-            <Card className="item-details">
-                <Card.Img variant="top" src={product.img} />
-                <Card.Body>
-                    <Card.Title>{product.name}</Card.Title>
-                    <Card.Text>
-                        {product.details}
-                    </Card.Text>
-                    <Button variant="success" onClick={handleAddProduct}>Agregar al carrito</Button>
-                </Card.Body>
-            </Card>
-    )
+    return <ItemDetail product={product} />
 }
